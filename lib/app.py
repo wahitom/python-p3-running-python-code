6ec61f753f029700d8663f2049f9d77eebacf082
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-20 15:05:13 +0300

    Completed

---
+++ lib/app.py
@@ -0,0 +1,11 @@
+# print("Hellow World", end=" ")
+# print ("Today is Monday 20th November 2023", end="!! ")
+# print("It is my first time coding in python", end="!!!\n")
+# print("yay it worked") 
+
+print("Hello World! Pass this test, please.")
+
+
+#print("Hello world!", end=" ")
+#print("Hello sun!", end="!! ")
+#print("Hello sky!", end="!!!\n")
